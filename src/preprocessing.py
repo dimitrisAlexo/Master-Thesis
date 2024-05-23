fb756dcaa1780877934dd3858b4219ee9351cb13
dimitrisAlexo <72296554+dimitrisAlexo@users.noreply.github.com>
2024-05-23 13:23:45 +0300

    create bags

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,19 +1,42 @@
+import numpy as np
 from utils import *
 import time
 import matplotlib.pyplot as plt
+import os
 
 start = time.time()
 
-tremor_sdata, tremor_gdata = unpickle_data("data/tremor_sdata.pickle", "data/tremor_gdata.pickle")
+# Adjust the paths to be relative to the current script location
+sdata_path = os.path.join('..', 'data', 'tremor_sdata.pickle')
+gdata_path = os.path.join('..', 'data', 'tremor_gdata.pickle')
+tremor_sdata, tremor_gdata = unpickle_data(sdata_path, gdata_path)
 
 print(tremor_sdata.keys())
 print(len(tremor_sdata['418d8c4cb78b2514'][3][0][0]))  # tremor_sdata[subject][index][call][segment][acceleration]
-print(tremor_sdata['418d8c4cb78b2514'][1])  # annotation
+print(tremor_sdata['6cc41389d3e9aea9'][1])  # annotation
 
+segment_energy = calculate_energy(tremor_sdata['418d8c4cb78b2514'][3][0][0])
+print(segment_energy)
 
+# subject = [np.array([[[0, 0, 0], [0, 0, 0], [1, 1, 1], [2, 2, 2]],
+#                     [[0, 0, 0], [0, 0, 0], [1, 1, 1], [0, 0, 0]],
+#                     [[0, 0, 0], [0, 0, 0], [1, 0, 1], [0, 0, 0]]]),
+#            np.array([[[0, 0, 0], [0, 0, 0], [1, 1, 1], [2, 2, 2]],
+#                      [[0, 0, 0], [0, 0, 0], [1, 1, 1], [5, 5, 5]],
+#                      [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]]),
+#            np.array([[[0, 0, 0], [0, 0, 0], [1, 1, 1], [1, 1, 1]],
+#                      [[0, 0, 0], [0, 0, 0], [1, 1, 1], [1, 1, 1]],
+#                      [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]])
+#            ]
+#
+# # print(subject)
+# print('energy', calculate_energy(np.array([[0, 0, 0], [0, 0, 0], [1, 1, 1], [5, 5, 5]])))
 
-# max_length = max([len(tremor_sdata[subject][3][i]) for subject in tremor_sdata.keys()
-#                   for i in range(len(tremor_sdata[subject][3]))])
-# print(max_length)
+bag = create_bag(tremor_sdata['6cc41389d3e9aea9'], 0.15, 1500)
+
+print('--------------------------------')
+print(bag)
+print(type(bag))
+print(len(bag))
 
 print(time.time() - start)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import pickle as pkl
+import scipy.signal
+import numpy as np
 
 
 def unpickle_data(sfilepath, gfilepath):
@@ -11,3 +13,31 @@ def unpickle_data(sfilepath, gfilepath):
         tremor_gdata = pkl.load(f)
 
     return tremor_sdata, tremor_gdata
+
+
+def calculate_energy(segment, fs=100, band=None):
+    if band is None:
+        band = [3, 7]
+    energies = []
+    for axis in range(segment.shape[1]):
+        f, Pxx = scipy.signal.welch(segment[:, axis], fs=fs)
+        band_energy = np.sum(Pxx[(f >= band[0]) & (f <= band[1])])
+        energies.append(band_energy)
+    return np.sum(energies)  # Sum energy across all axes
+
+
+def create_bag(subject, E_thres, Kt):
+    bag = []
+    for session in subject[3]:
+        filtered_session = [segment for segment in session if calculate_energy(segment) > E_thres]
+        if len(filtered_session) >= 2:
+            bag.extend([segment for segment in filtered_session])
+    bag.sort(key=lambda segment: calculate_energy(segment), reverse=True)
+    bag = bag[:Kt]
+
+    # Zero-padding if less than Kt segments
+    if len(bag) < Kt:
+        padding = [np.zeros((500, 3)) for _ in range(Kt - len(bag))]
+        bag.extend(padding)
+
+    return np.array(bag)
