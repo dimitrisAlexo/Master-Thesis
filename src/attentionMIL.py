85675a15878e14599310950613a85ef05b335334
dimitris_alexo <dimalexopoulos01@gmail.com>
2024-05-31 00:26:14 +0300

    MIL stable

--- src/attentionMIL.py
+++ src/attentionMIL.py
@@ -3,15 +3,33 @@ import keras
 from keras import layers
 from keras import ops
 from tqdm import tqdm
-from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
 
-plt.style.use("ggplot")
+from utils import *
+import os
+import time
 
-POSITIVE_CLASS = 1
-BAG_COUNT = 1000
-VAL_BAG_COUNT = 300
-BAG_SIZE = 3
-PLOT_SIZE = 3
+start = time.time()
+
+"""
+## Create dataset
+
+We will create a set of bags and assign their labels according to their contents.
+If at least one positive instance
+is available in a bag, the bag is considered as a positive bag. If it does not contain any
+positive instance, the bag will be considered as negative.
+
+### Configuration parameters
+
+- `POSITIVE_CLASS`: The desired class to be kept in the positive bag.
+- `BAG_COUNT`: The number of training bags.
+- `VAL_BAG_COUNT`: The number of validation bags.
+- `BAG_SIZE`: The number of instances in a bag.
+- `PLOT_SIZE`: The number of bags to plot.
+- `ENSEMBLE_AVG_COUNT`: The number of models to create and average together. (Optional:
+often results in better performance - set to 1 for single model)
+"""
+
 ENSEMBLE_AVG_COUNT = 1
 
 
@@ -30,12 +48,12 @@ class MILAttentionLayer(layers.Layer):
     """
 
     def __init__(
-        self,
-        weight_params_dim,
-        kernel_initializer="glorot_uniform",
-        kernel_regularizer=None,
-        use_gated=False,
-        **kwargs,
+            self,
+            weight_params_dim,
+            kernel_initializer="glorot_uniform",
+            kernel_regularizer=None,
+            use_gated=False,
+            **kwargs,
     ):
         super().__init__(**kwargs)
 
@@ -115,3 +133,211 @@ class MILAttentionLayer(layers.Layer):
 
         # w^T*(tanh(v*h_k^T)) / w^T*(tanh(v*h_k^T)*sigmoid(u*h_k^T))
         return ops.tensordot(instance, self.w_weight_params, axes=1)
+
+
+def create_model(instance_shape):
+    # Extract features from inputs.
+    inputs, embeddings = [], []
+    shared_dense_layer_1 = layers.Dense(128, activation="relu")
+    shared_dense_layer_2 = layers.Dense(64, activation="relu")
+    for _ in range(Kt):
+        inp = layers.Input(instance_shape)
+        flatten = layers.Flatten()(inp)
+        dense_1 = shared_dense_layer_1(flatten)
+        dense_2 = shared_dense_layer_2(dense_1)
+        inputs.append(inp)
+        embeddings.append(dense_2)
+
+    # Invoke the attention layer.
+    alpha = MILAttentionLayer(
+        weight_params_dim=256,
+        kernel_regularizer=keras.regularizers.L2(0.01),
+        use_gated=True,
+        name="alpha",
+    )(embeddings)
+
+    # Multiply attention weights with the input layers.
+    multiply_layers = [
+        layers.multiply([alpha[i], embeddings[i]]) for i in range(len(alpha))
+    ]
+
+    # Concatenate layers.
+    concat = layers.concatenate(multiply_layers, axis=1)
+
+    # Classification output node.
+    output = layers.Dense(2, activation="softmax")(concat)
+
+    # print(type(inputs))
+    # print(np.shape(inputs))
+    # print(type(output))
+    # print(np.shape(output))
+
+    return keras.Model(inputs, output)
+
+
+def compute_class_weights(labels):
+    # Count number of postive and negative bags.
+    negative_count = len(np.where(labels == 0)[0])
+    positive_count = len(np.where(labels == 1)[0])
+    total_count = negative_count + positive_count
+
+    # Build class weight dictionary.
+    return {
+        0: (1 / negative_count) * (total_count / 2),
+        1: (1 / positive_count) * (total_count / 2),
+    }
+
+
+def train(train_data, train_labels, val_data, val_labels, model):
+    # Train model.
+    # Prepare callbacks.
+    # Path where to save best weights.
+
+    # Take the file name from the wrapper.
+    file_path = "/tmp/best_model.weights.h5"
+
+    # Initialize model checkpoint callback.
+    model_checkpoint = keras.callbacks.ModelCheckpoint(
+        file_path,
+        monitor="val_loss",
+        verbose=0,
+        mode="min",
+        save_best_only=True,
+        save_weights_only=True,
+    )
+
+    # Initialize early stopping callback.
+    # The model performance is monitored across the validation data and stops training
+    # when the generalization error cease to decrease.
+    early_stopping = keras.callbacks.EarlyStopping(
+        monitor="val_loss", patience=10, mode="min"
+    )
+
+    # Compile model.
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    # Fit model.
+    model.fit(
+        train_data,
+        train_labels,
+        validation_data=(val_data, val_labels),
+        epochs=30,
+        class_weight=compute_class_weights(train_labels),
+        batch_size=1,
+        callbacks=[early_stopping, model_checkpoint],
+        verbose=0,
+    )
+
+    # Load best weights.
+    model.load_weights(file_path)
+
+    return model
+
+
+# Adjust the paths to be relative to the current script location
+sdata_path = os.path.join('..', 'data', 'tremor_sdata.pickle')
+gdata_path = os.path.join('..', 'data', 'tremor_gdata.pickle')
+tremor_sdata, tremor_gdata = unpickle_data(sdata_path, gdata_path)
+
+E_thres = 0.15
+Kt = 500
+sdataset = form_dataset(tremor_sdata, E_thres, Kt)
+
+# Split the dataset into training and validation sets with an 80/20 split
+train_df, val_df = train_test_split(sdataset, test_size=0.2, random_state=42)
+
+# Extract lists and convert labels to the desired format
+train_data = np.array([np.array(instance) for instance in train_df['X'].tolist()])
+train_data = list(np.transpose(train_data, (1, 0, 2, 3)))
+train_labels = np.array([np.array([label]) for label in train_df['y'].tolist()])
+
+val_data = np.array([np.array(instance) for instance in val_df['X'].tolist()])
+val_data = list(np.transpose(val_data, (1, 0, 2, 3)))
+val_labels = np.array([np.array([label]) for label in val_df['y'].tolist()])
+
+# Print lengths to verify the split
+print(f'Training data size: {np.shape(np.array(train_data))}')
+
+# train_data = list(np.transpose(train_data, (1, 0, 2, 3)))
+
+print(f'Training data: {np.shape(np.array(train_data))}')
+
+print(f'Training labels: {np.shape(train_labels)}')
+print(f'Validation data: {len(val_data)}')
+print(f'Validation labels: {val_labels.shape}')
+
+print(type(train_data))
+print(type(train_data[0]))
+print(type(train_data[0][0]))
+print(type(train_labels))
+print(type(train_labels[0]))
+print(np.shape(train_data))
+print(np.shape(train_labels))
+
+# # Print the splits
+# print(f'Training data: {train_data}')
+# print(f'Training labels: {train_labels}')
+# print(f'Validation data: {val_data}')
+# print(f'Validation labels: {val_labels}')
+
+# Building model(s).
+instance_shape = train_data[0][0].shape
+print(instance_shape)
+models = [create_model(instance_shape) for _ in range(ENSEMBLE_AVG_COUNT)]
+
+# print(models[0].input)
+# print(models[0].output)
+
+# # Show single model architecture.
+# print(models[0].summary())
+
+# Training model(s).
+trained_models = [
+    train(train_data, train_labels, val_data, val_labels, model)
+    for model in tqdm(models)
+]
+
+def predict(data, labels, trained_models):
+    # Collect info per model.
+    models_predictions = []
+    models_attention_weights = []
+    models_losses = []
+    models_accuracies = []
+
+    for model in trained_models:
+        # Predict output classes on data.
+        predictions = model.predict(data)
+        models_predictions.append(predictions)
+
+        # Create intermediate model to get MIL attention layer weights.
+        intermediate_model = keras.Model(model.input, model.get_layer("alpha").output)
+
+        # Predict MIL attention layer weights.
+        intermediate_predictions = intermediate_model.predict(data)
+
+        attention_weights = np.squeeze(np.swapaxes(intermediate_predictions, 1, 0))
+        models_attention_weights.append(attention_weights)
+
+        loss, accuracy = model.evaluate(data, labels, verbose=0)
+        models_losses.append(loss)
+        models_accuracies.append(accuracy)
+
+    print(
+        f"The average loss and accuracy are {np.sum(models_losses, axis=0) / ENSEMBLE_AVG_COUNT:.2f}"
+        f" and {100 * np.sum(models_accuracies, axis=0) / ENSEMBLE_AVG_COUNT:.2f} % resp."
+    )
+
+    return (
+        np.sum(models_predictions, axis=0) / ENSEMBLE_AVG_COUNT,
+        np.sum(models_attention_weights, axis=0) / ENSEMBLE_AVG_COUNT,
+    )
+
+
+# Evaluate and predict classes and attention scores on validation data.
+class_predictions, attention_params = predict(val_data, val_labels, trained_models)
+
+print(time.time() - start)
